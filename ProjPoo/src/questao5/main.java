6d8912af6ad7c7d04c12d69a286542dc05f79823
adriellyEster <adriellyester9@gmail.com>
2022-10-01 21:03:36 -0300

    commit do projeto da disciplica de POO

---
+++ ProjPoo/src/questao5/main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package questao5;
+
+/**
+ *
+ * @author adrielly
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void start(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ola mundo");
+    }
+    
+}
